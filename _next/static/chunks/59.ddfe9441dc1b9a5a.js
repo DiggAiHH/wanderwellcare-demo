d3fa3d05f0d29c5ff5918c639a596ac2823686f9
DiggAiHH <wanderwellcare@gmail.com>
2026-07-02 15:18:18 +0200

    FAQ page + tap targets

---
+++ _next/static/chunks/59.ddfe9441dc1b9a5a.js
@@ -0,0 +1,11 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[59],{3231:function(e,c,n){n.d(c,{Z:function(){return t}});/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let t=(0,n(8030).Z)("CircleCheck",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]])},3274:function(e,c,n){n.d(c,{Z:function(){return t}});/**
+ * @license lucide-react v0.383.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */let t=(0,n(8030).Z)("LoaderCircle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]])}}]);
